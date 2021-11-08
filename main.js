87322697527d87fa5dc129d0d8485332fa83fbab
unknown <saadbinosama@gmail.com>
2021-11-08 21:57:16 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+const myform = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myform.addEventListener('submit' , onSubmit);
+
+function onSubmit(e)  {
+
+e.preventDefault();
+
+if(nameInput.value==='' || emailInput===''){
+    msg.classList.add('error');
+   
+    msg.innerHTML = 'Please enter all fields';
+setTimeout(() => msg.remove(), 3000)}
+
+else {;
+const li = document.createElement('li');
+li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+
+userList.appendChild(li);
+nameInput.value = '';
+emailInput.value = '';
+}
+}
